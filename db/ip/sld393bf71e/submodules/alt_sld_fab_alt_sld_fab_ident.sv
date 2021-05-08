be8bfc0e711e932b3c6a5fd01d1c13696b3e3219
Pablo Martinez Sanchez <pabmars2@teleco.upv.es>
2021-05-08 10:32:43 +0200

    Debug Mode changed
    No more "waiting for instruccion to write in the memory" on initial boot

--- db/ip/sld393bf71e/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sld393bf71e/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : d7fedc0652b4f342b5e0
+//   DESIGN_HASH   : a4fe2755908adfaec176
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hd7fedc0652b4f342b5e0 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha4fe2755908adfaec176 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
